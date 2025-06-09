09daad4309815eafc02014b3c3386100b5fd2b58
sahvsergio <sergioandresherrera@gmail.com>
2025-06-09 11:20:53 -0500

    Create All the component files for the project

---
+++ client/src/components/CourseDetail.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const CourseDetail=()=>{
+    return(
+
+
+      
+            <a class="course--module course--link" href="course-detail.html">
+                <h2 class="course--label">Course</h2>
+                <h3 class="course--title">Build a Basic Bookcase</h3>
+            </a>
+
+            )
+
+
+
+
+}
+
+export default CourseDetail;
