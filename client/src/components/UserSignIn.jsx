3930278993aa26ed77a17ad59f4eb4001a871a46
sahvsergio <sergioandresherrera@gmail.com>
2025-06-30 09:27:43 -0500

    attempt UserContext

--- client/src/components/UserSignIn.jsx
+++ client/src/components/UserSignIn.jsx
@@ -1,103 +1,92 @@
 import React from "react"
-import { useRef, useState } from 'react';
+import { useRef, useState, useContext } from 'react';
 
 import { Link, useNavigate } from 'react-router-dom';
 import { api } from '../apiHelper';
+import UserContext from "../context/UserContext";
 
-const UserSignIn=()=>{
-    const navigate = useNavigate()
 
-    const email = useRef(null);
-    const password = useRef(null);
-     const[errors, setErrors]=useState([]);
-    const handleSubmit=  (event )=>{
-        event.preventDefault();
-        const credentials={
-            emailAddress: email.current.value,
-            password: password.current.value
 
-        }
 
-         try{
-          
-           let response = api('/users', 'GET', null, credentials);
-           response.then((responseData)=>{
-            if(responseData){
-            if (responseData.status===200){
-            
-             let user=responseData.json();
-             user.then((userData)=>{
-              console.log(`${userData.firstName} ${userData.lastName} is now signed in`)
-              navigate('/authenticated')
-             })
-            
-            }
-            else if(responseData.status===401){
-              setErrors(['Sign In is unsuccessful'])
-              
-            }
-              
-            
-            
-              else{
-            throw new Error('Something wrong happened')
-          }
-        }
-      })
-     }catch (error) {
-         
-      console.log(error);
-      navigate('/error');
-    }
+
+const UserSignIn = () => {
+
+  const {actions} =useContext(UserContext);
+  const navigate = useNavigate()
+  const email = useRef(null);
+  const password = useRef(null);
+  const [errors, setErrors] = useState([]);
+
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+
+
+  const credentials = {
+    emailAddress: email.current.value,
+    password: password.current.value
+  }
+
+  try {
+    const user=actions.UserSignIn(credentials);
+    user.then((userData)=>{
+
+      console.log(userData.json());
+    })
+    
+ 
+    //naviga
+
+
+  } catch (error) {
+
+    console.log(error);
+    //navigate('/error');
+  }
+}
+
+const handleCancel = (event) => {
+  event.preventDefault();
+  navigate('/')
+
 }
 
 
-       
-          
-       
-
-    const handleCancel = (event) => {
-        event.preventDefault();
-        navigate('/')
-
-      }
-  
-  
-    return(<>
-       <main>
-        <div className="form--centered">
-            <h2>Sign In</h2>
-                {errors.length ? (
-                    <div className="validation--errors">
-                        <h3 className="validation--errors">Validation errors</h3>
-                        <ul className="validation--errors">
-                            {errors.map((error, i) => <li className="validation--errors" key={i}>{error}</li>)}
-                        </ul>
-                    </div>
-
-                ):(<div></div>)}
-                <form onSubmit={handleSubmit}>
-                <label htmlFor="emailAddress">Email Address</label>
-                <input 
-                id="emailAddress" 
-                name="emailAddress"
-                type="email"
-                ref={email}/>
-                <label htmlFor="password">Password</label>
-                <input 
-                id="password" 
-                name="password" 
-                type="password" 
-                ref={password}/>
-                <button className="button" type="submit">Sign In</button>
-                    <button className="button button-secondary" onClick={handleCancel} >Cancel</button>
-            </form>
-
+return (<>
+  <main>
+    <div className="form--centered">
+      <h2>Sign In</h2>
+      {errors.length ? (
+        <div className="validation--errors">
+          <h3 className="validation--errors">Validation errors</h3>
+          <ul className="validation--errors">
+            {errors.map((error, i) => <li className="validation--errors" key={i}>{error}</li>)}
+          </ul>
         </div>
 
+      ) : (<div></div>)}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="emailAddress">Email Address</label>
+        <input
+          id="emailAddress"
+          name="emailAddress"
+          type="email"
+          ref={email} />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          ref={password} />
+        <button className="button" type="submit">Sign In</button>
+        <button className="button button-secondary" onClick={handleCancel} >Cancel</button>
+      </form>
 
-       </main>
-    </>
-    )}
+    </div>
+
+
+  </main>
+</>
+)}
 
 export default UserSignIn;
---
+++ client/src/context/UserContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+const UserContext = createContext(null);
+import { api } from "../apiHelper";
+
+
+
+
+export const UserProvider = (props) => {
+  const [authUser, setAuthUser] = useState(null);
+  const signIn = (credentials) => {
+
+    let response = api("/users", "GET", null, credentials);
+
+    response.then((responseData) => {
+
+      if (responseData) {
+
+        if (responseData.status === 200) {
+          let user = responseData.json();
+          user.then((userData) => {
+            setAuthUser(userData);
+            return userData.json();
+          });
+        } else if (responseData.status === 401) {
+          return null;
+        }
+    }});
+
+  }
+
+
+return (
+  <UserContext.Provider value={{
+    authUser,
+    actions: {
+      signIn
+    }
+  }}>
+    {props.children}
+  </UserContext.Provider>
+)
+}
+export default UserContext  
