1bbd1f201834d116bf399e61c86a990f76df695b
sahvsergio <sergioandresherrera@gmail.com>
2025-06-30 20:34:41 -0500

    refactor Main.jsx

---
+++ client/src/Main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { UserProvider } from "./context/UserContext.jsx";
+
+import "./styles/reset.css";
+import "./styles/global.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </StrictMode>
+);
