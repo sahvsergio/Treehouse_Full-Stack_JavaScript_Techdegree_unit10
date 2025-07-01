8ecbf4670fd9491ad5a19b0b4deeebfec288aefa
sahvsergio <sergioandresherrera@gmail.com>
2025-06-30 23:02:18 -0500

    Use htmlFor and className as replacements to regular html attributes on CreateCourse Component

--- client/src/components/CreateCourse.jsx
+++ client/src/components/CreateCourse.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const CreateCourse = () => {
     
+    
+    
     return (
     <main>
-            <div class="wrap">
+            <div className="wrap">
                 <h2>Create Course</h2>
-                <div class="validation--errors">
+                <div className="validation--errors">
                     <h3>Validation Errors</h3>
                     <ul>
                         <li>Please provide a value for "Title"</li>
@@ -14,25 +16,25 @@ const CreateCourse = () => {
                     </ul>
                 </div>
                 <form>
-                    <div class="main--flex">
+                    <div className="main--flex">
                         <div>
-                            <label for="courseTitle">Course Title</label>
+                            <label htmlFor="courseTitle">Course Title</label>
                             <input id="courseTitle" name="courseTitle" type="text" value=""/>
 
                                 <p>By Joe Smith</p>
 
-                                <label for="courseDescription">Course Description</label>
+                                <label htmlFor="courseDescription">Course Description</label>
                                 <textarea id="courseDescription" name="courseDescription"></textarea>
                         </div>
                         <div>
-                            <label for="estimatedTime">Estimated Time</label>
+                            <label htmlFor="estimatedTime">Estimated Time</label>
                             <input id="estimatedTime" name="estimatedTime" type="text" value=""/>
 
-                                <label for="materialsNeeded">Materials Needed</label>
+                                <label htmlFor="materialsNeeded">Materials Needed</label>
                                 <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
                         </div>
                     </div>
-                    <button class="button" type="submit">Create Course</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                    <button className="button" type="submit">Create Course</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
                 </form>
             </div>
         </main>
