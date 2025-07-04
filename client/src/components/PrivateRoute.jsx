18abd4a678ffad91b6d0fb6e1254f4d0c92bae67
sahvsergio <sergioandresherrera@gmail.com>
2025-07-04 15:24:00 -0500

    Nest CreateCourse component inside PrivateRoute

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -4,6 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 import UserContext from "../context/UserContext";
 
+
 const PrivateRoute= ()=>{
     const {authUser}=useContext(UserContext);
     if(authUser){
